10a0bcd434e9d63c8139cdf72d78e2b0d6f8513d
Huan-Chieh Liao <liao.jason2@gmail.com>
2021-11-06 22:57:28 +0800

    Add Components

--- func/welcome.py
+++ func/welcome.py
@@ -14,24 +14,30 @@ from linebot.models.actions import PostbackAction
 from linebot.models.flex_message import BubbleContainer, BoxComponent, TextComponent, ButtonComponent
 load_dotenv()
 
-line_bot_api = LineBotApi(os.getenv("CHANNEL_TOKEN"))
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_TOKEN"))
 
 def welcomeMenu(event):
     flex_message = FlexSendMessage(
         alt_text="menu",
         contents=BubbleContainer(
-            body=BoxComponent(
+            header=BoxComponent(
                 layout="vertical",
                 contents=[
                     TextComponent(text="請選擇操作", align="center"),
                 ],
             ),
-            footer=BoxComponent(
+            body=BoxComponent(
                 layout="vertical",
                 contents=[
                     ButtonComponent(
                         action=PostbackAction(label="記帳", data="add"),
-                    ),
+                    ),                
+                ],
+            ),
+            footer=BoxComponent(
+                layout="vertical",
+                contents=[
+                    TextComponent(text="請選擇操作", align="center"),
                 ],
             ),
         ),
