dd559efcaecf08a5a5a44c85035065f57bf04797
Jason Liao <liao.jason2@gmail.com>
2025-02-21 01:47:14 +0800

    Remove unnessary code

--- func/addData.py
+++ func/addData.py
@@ -38,12 +38,6 @@ def passUserTypedAmountToConfirmMenu(userId, event, tempData):
     amount = int(float(amount) * float(exchangeRate))
     # Covert to string for showing prompt_message
     amount = str(amount)
-    # Define prompt_message to confirm section
-    prompt_message = '請確認是否要將 ' + amount + " 的 " + subject + "加入資料庫中"
-    if (exchangeRate != 1.0):
-        prompt_message = '請確認是否要將 ' + amount + " 的 " + \
-            subject + " 加入資料庫中（匯率 " + str(exchangeRate) + "）。"
-    # Pass to confirmAmount section
     confirmAmount(category, subject, amount, exchangeRate, replyToken, configuration)
 
 def sendReplyMessage(line_bot_api, reply_token, message_text):
