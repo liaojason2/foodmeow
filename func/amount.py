c16e8ecb48a2e290294ab0afe4326ff4f37a6f89
Jason Liao <liao.jason2@gmail.com>
2025-05-07 14:01:17 +0800

    Edit giveAmount function to support new and old data type

--- func/amount.py
+++ func/amount.py
@@ -14,6 +14,37 @@ conn = MongoClient(os.getenv("MONGODB_CONNECTION"))
 db = conn.foodmeow
 data = db.data
 
+def convertAmountToCent(amount):
+    """
+    Convert the amount to cents.
+
+    :param amount: The amount in dollars (e.g., 140.00)
+    :return: The amount in cents (e.g., 14000)
+    """
+    if type(amount) == float:
+        return int(amount * 100)
+    if type(amount) == str:
+        if '.' in amount:
+            dollars, cents = amount.split('.')
+            cents = (cents + '00')[:2]  # ensure 2 digits
+            return int(dollars) * 100 + int(cents)
+        else:
+            return int(amount) * 100
+        
+def convertCentToDecimalString(amount):
+    """
+    Convert the amount in cents to a decimal string.
+
+    :param amount: The amount in cents (e.g., 14000)
+    :return: The amount in dollars as a string (e.g., "140.00")
+    """
+    if type(amount) == int:
+        dollars = amount // 100
+        cents = amount % 100
+        return f"{dollars}.{cents:02d}"
+    else:
+        raise ValueError("Amount must be an integer.")
+
 def insertData(subject: str, baseAmount: int, addition: int, total: int, category: str):
     try:
         data.insert_one(
@@ -30,13 +61,18 @@ def insertData(subject: str, baseAmount: int, addition: int, total: int, categor
     except Exception as e:
         return e
 
-
 def getTotalAmount():
-    totalAmount = 0.0
+    totalAmount = 0
     foods = data.find()
     for amount in foods:
         money = amount['total']
-        totalAmount += float(money)
+        # Compatible with old data added < v1.2.1
+        if isinstance(money, float) == True:
+            totalAmount += int(money*100)
+        # Version 1.2.2 and later use int to store cent  
+        else:
+            totalAmount += money
+    totalAmount = convertCentToDecimalString(totalAmount)
     return totalAmount
 
 
