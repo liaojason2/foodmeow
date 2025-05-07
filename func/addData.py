9ad064ca85ddc493ecbb77d693405ed26f9938fe
Jason Liao <liao.jason2@gmail.com>
2025-05-07 15:17:28 +0800

    Move convert int decimal function to utils

--- func/addData.py
+++ func/addData.py
@@ -18,6 +18,7 @@ from .menu import selectDataCategory, confirmAmount, addDataSuccess
 from . import amount
 from .amount import insertData
 from .config import getFoodMultiple
+from .utils import convertAmountToCent, convertCentToDecimalString
 
 load_dotenv()
 
@@ -32,37 +33,6 @@ def sendReplyMessage(line_bot_api, reply_token, message_text):
             messages=[TextMessage(text=message_text)]
         )
     )
-
-def convertAmountToCent(amount):
-    """
-    Convert the amount to cents.
-
-    :param amount: The amount in dollars (e.g., 140.00)
-    :return: The amount in cents (e.g., 14000)
-    """
-    if type(amount) == float:
-        return int(amount * 100)
-    if type(amount) == str:
-        if '.' in amount:
-            dollars, cents = amount.split('.')
-            cents = (cents + '00')[:2]  # ensure 2 digits
-            return int(dollars) * 100 + int(cents)
-        else:
-            return int(amount) * 100
-        
-def convertCentToDecimalString(amount):
-    """
-    Convert the amount in cents to a decimal string.
-
-    :param amount: The amount in cents (e.g., 14000)
-    :return: The amount in dollars as a string (e.g., "140.00")
-    """
-    if type(amount) == int:
-        dollars = amount // 100
-        cents = amount % 100
-        return f"{dollars}.{cents:02d}"
-    else:
-        raise ValueError("Amount must be an integer.")
 
 with ApiClient(configuration) as api_client:
 
